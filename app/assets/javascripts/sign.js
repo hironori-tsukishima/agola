ff795c5d2358de8a0135430ce8199e217a0c5cf5
tsukishima <hir01994o915@gmail.com>
2018-12-06 11:21:53 +0900

    sign時のjs

---
+++ app/assets/javascripts/sign.js
@@ -0,0 +1,10 @@
+$(window).on("scroll", function(){
+  console.log("クルー")
+  $("#en1").hide().fadeIn(500);
+  $("#en2").hide().fadeIn(1000);
+  $("#en3").hide().fadeIn(1500);
+  $("#en4").hide().fadeIn(2000);
+  $("#en5").hide().fadeIn(2500);
+  $("#en6").hide().fadeIn(3000);
+  $("#en7").hide().fadeIn(3500);
+});
